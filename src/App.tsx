d6412ba0dded9c506e90b3062315e0174d654a4f
Mohit Lakhara <mohitlakhara78@gmail.com>
2024-10-28 21:04:51 +0530

    Update App.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import Navbar from "./components/Navbar.jsx";
+import TxtSpace from "./components/TxtSpace.jsx";
+import Alert from "./components/Alert.jsx";
+import AboutUs from "./components/AboutUs.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState } from "react";
 function App() {
-  const [count, setCount] = useState(0)
+  const [mode, setMode] = useState("light");
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
 
+      document.body.style.backgroundColor = "#1a1c1e";
+      document.body.style.color = "#ffffff";
+      showAlert("Dark Mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "#ffffff";
+      document.body.style.color = "#1a1c1e";
+      showAlert("Light Mode has been enabled", "success");
+    }
+  };
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1000);
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Navbar mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+
+        <Routes>
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route
+            path="/"
+            element={
+              <TxtSpace
+                label="Enter Your text Here"
+                mode={mode}
+                showAlert={showAlert}
+              />
+            }
+          />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
